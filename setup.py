541514dc94a5378871f3050c3e67dacee2541f79
bolmez <burhan.olmezz@gmail.com>
2021-02-27 13:16:48 +0300

    setup file is created

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='gym_foo',
+setup(name='AerialManipulatorDRL',
       version='0.0.1',
       install_requires=['gym', 'tensorflow', 'stable-baselines', 'numpy']  # And any other dependencies foo needs
       )
